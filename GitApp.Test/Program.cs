83caecfd3296731204ce8e515ebd92ca80b9246b
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2018-08-12 00:00:47 +0800

    Add project files.

---
+++ GitApp.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitApp.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

8dcdf39f5e9f8eb5e3042d4d23ca5bea94d7fe6d
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2018-08-12 00:34:07 +0800

    gege111

---
+++ GitApp.Test/Hehe/Start.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitApp.Test.Hehe
+{
+    class Start
+    {
+        public void Run()
+        {
+            Console.WriteLine("Heheda");
+        }
+    }
+}

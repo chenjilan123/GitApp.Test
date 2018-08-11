23c4ab91b0b520641ee33f5218e7727cb4913846
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2018-08-12 00:08:51 +0800

    Update Program.cs

--- GitApp.Test/Program.cs
+++ GitApp.Test/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 
 namespace GitApp.Test
 {
@@ -7,6 +8,8 @@ namespace GitApp.Test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            //呵呵哒
         }
     }
 }
